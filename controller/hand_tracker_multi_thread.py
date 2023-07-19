af3615ab7f21506ee7ef7790e0c1120a1e476c8f
Mike <currymike@gmail.com>
2023-07-19 15:25:49 -0400

    Multi Threading Hand Tracking

--- controller/hand_tracker_multi_thread.py
+++ controller/hand_tracker_multi_thread.py
@@ -21,22 +21,21 @@ serversocket.listen(5)
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-# List of active connections
-connections = []
+# For webcam input:
+cap = cv2.VideoCapture(0)
 
-def client_handler(clientsocket):
-    # Are there multiple hands?
-    multiple_hands = -1
+# Define a ClientHandler class
+class ClientHandler(threading.Thread):
+    def __init__(self, clientsocket):
+        threading.Thread.__init__(self)
+        self.clientsocket = clientsocket
+        self.center_x = 0
+        self.center_y = 0
+        self.multiple_hands = -1
+        self.hands = mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-    # Hand coordinates
-    center_x = 0
-    center_y = 0
-
-    # For webcam input:
-    cap = cv2.VideoCapture(0)
-
-    with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
-        while cap.isOpened():
+    def run(self):
+        while True:
             success, image = cap.read()
             if not success:
                 print("Ignoring empty camera frame.")
@@ -49,7 +48,7 @@ def client_handler(clientsocket):
             # To improve performance, optionally mark the image as not writeable to
             # pass by reference.
             image.flags.writeable = False
-            results = hands.process(image)
+            results = self.hands.process(image)
 
             # Draw the hand annotations on the image.
             image.flags.writeable = True
@@ -57,21 +56,11 @@ def client_handler(clientsocket):
             if results.multi_hand_landmarks:
                 if len(results.multi_hand_landmarks) > 1:
                     print("Multiple hands detected, please keep one hand behind.")
-                    multiple_hands = 1
-
-                    # send hand positions via server.
-                    msg = f'{center_x},{center_y},{multiple_hands}'
-                    try:
-                        clientsocket.send(msg.encode('ascii'))
-                    except BrokenPipeError:
-                        clientsocket.close()
-                        connections.remove(clientsocket)
-                        print("BrokenPipeError occurred, client disconnected prematurely.")
-                        break
+                    self.multiple_hands = 1
 
                 else:
                     hand_landmarks = results.multi_hand_landmarks[0]
-                    multiple_hands = 0
+                    self.multiple_hands = 0
                     # Find the bounding box coordinates
                     lm_list = []
                     for id, lm in enumerate(hand_landmarks.landmark):
@@ -82,32 +71,20 @@ def client_handler(clientsocket):
                     # Get the center of the hand
                     x_min, x_max = np.min(np.array(lm_list)[:,1]), np.max(np.array(lm_list)[:,1])
                     y_min, y_max = np.min(np.array(lm_list)[:,2]), np.max(np.array(lm_list)[:,2])
-                    center_x, center_y = (x_min + x_max) // 2, (y_min + y_max) // 2
-                    print(f'Hand center: x={center_x}, y={center_y}')
+                    self.center_x, self.center_y = (x_min + x_max) // 2, (y_min + y_max) // 2
+                    print(f'Hand center: x={self.center_x}, y={self.center_y}')
 
-                    # send hand positions via server.
-                    msg = f'{center_x},{center_y},{multiple_hands}'
-                    try:
-                        clientsocket.send(msg.encode('ascii'))
-                    except BrokenPipeError:
-                        clientsocket.close()
-                        connections.remove(clientsocket)
-                        print("BrokenPipeError occurred, client disconnected prematurely.")
-                        break
-                        
-            if cv2.waitKey(5) & 0xFF == 27:
-                break
-
-    cap.release()
+                # send hand positions via server.
+                msg = f'{self.center_x},{self.center_y},{self.multiple_hands}\n'
+                try:
+                    self.clientsocket.send(msg.encode('ascii'))
+                except BrokenPipeError:
+                    self.clientsocket.close()
+                    print("BrokenPipeError occurred, client disconnected prematurely.")
 
 while True:
     # establish a connection
     clientsocket, addr = serversocket.accept()
 
     print("Got a connection from %s" % str(addr))
-    connections.append(clientsocket)
-
-    client_thread = threading.Thread(target=client_handler, args=(clientsocket,))
-    client_thread.start()
-
-cv2.destroyAllWindows()
+    ClientHandler(clientsocket).start()
